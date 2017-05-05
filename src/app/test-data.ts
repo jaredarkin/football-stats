097bbea58fbf67b3c9f137f834552efc2163edbb
jaredarkin <ARKINJB@GMAIL.COM>
2017-05-05 14:31:11 -0400

    updated test data

---
+++ src/app/test-data.ts
@@ -0,0 +1,120 @@
+[
+    {   year: '2016',
+        overallRecords: 
+        [
+            { name: 'Adam L',
+            percentage: 53.68
+            },
+            { name: 'Bob G',
+            percentage: 63.10
+            },
+            { name: 'Danny S',
+            percentage: 49.44
+            },
+            { name: 'David A',
+            percentage: 57.83
+            },
+            { name: 'Eric H',
+            percentage: 51.61
+            },
+            { name: 'Howard M',
+            percentage: 57.14
+            },
+            { name: 'Jared A',
+            percentage: 53.61
+            },
+            { name: 'Jeff J',
+            percentage: 46.67
+            },
+            { name: 'John H',
+            percentage: 46.84
+            },
+            { name: 'Josh A',
+            percentage: 55.06
+            },
+            { name: 'Josh W',
+            percentage: 53.66
+            },
+            { name: 'Kate G',
+            percentage: 44.44
+            },
+            { name: 'Lori A',
+            percentage: 48.84
+            },
+            { name: 'Matt W',
+            percentage: 40.70
+            },
+            { name: 'May D',
+            percentage: 52.70
+            },
+            { name: 'Michael M',
+            percentage: 50.00
+            },
+            { name: 'Paul W',
+            percentage: 51.90
+            }
+        ]
+    },
+    {   year: '2015',
+        overallRecords: 
+        [
+            { name: 'Adam L',
+            percentage: 60.53
+            },
+            { name: 'Anthony M',
+            percentage: 42.17
+            },
+            { name: 'Bob G',
+            percentage: 47.37
+            },
+            { name: 'Brett A',
+            percentage: 46.67
+            },
+            { name: 'Danny S',
+            percentage: 52.69
+            },
+            { name: 'David A',
+            percentage: 47.06
+            },
+            { name: 'Eric H',
+            percentage: 50.50
+            },
+            { name: 'Howard M',
+            percentage: 52.87
+            },
+            { name: 'Jared A',
+            percentage: 51.52
+            },
+            { name: 'Jeff J',
+            percentage: 55.84
+            },
+            { name: 'John H',
+            percentage: 53.66
+            },
+            { name: 'Josh A',
+            percentage: 51.25
+            },
+            { name: 'Josh W',
+            percentage: 57.14
+            },
+            { name: 'Kate G',
+            percentage: 54.12
+            },
+            { name: 'Lori A',
+            percentage: 54.65
+            },
+            { name: 'Maria P',
+            percentage: 43.21
+            },
+            { name: 'Matt W',
+            percentage: 44.90
+            },
+            { name: 'May D',
+            percentage: 44.87
+            },
+            { name: 'Paul W',
+            percentage: 48.05
+            }
+        ]
+    }
+]
